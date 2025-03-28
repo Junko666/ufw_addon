492a08de3871472899a1b7495f720cf3416641e0
Junko <126285554+Junko666@users.noreply.github.com>
2025-03-28 15:10:52 +0100

    Update run.sh

--- addon/run.sh
+++ addon/run.sh
@@ -3,5 +3,6 @@ set -e
 
 ufw default deny incoming
 ufw allow from 192.168.0.2 to any port 8123 proto tcp
+ufw allow from 192.168.0.2 to any port 443 proto tcp
 ufw allow from 192.168.0.2 to any port 22 proto tcp
 ufw --force enable
